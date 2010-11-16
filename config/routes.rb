d4563d90fb50e07e3615b94a9f9423d5b2428499
Andrew Shu <talklittle@gmail.com>
2010-11-16 12:38:35 -0500

    update some pages, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :tasks
 
-  map.resources :users
-
-  map.resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -41,15 +37,21 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
+  map.register  '/register',  :controller => 'users', :action => 'new'
+  map.profile   '/profile',   :controller => 'users', :action => 'edit'
+  map.tasks     '/tasks',     :controller => 'tasks', :action => 'index'
+  map.about     '/about',     :controller => 'pages', :action => 'about'
+  map.contact   '/contact',   :controller => 'pages', :action => 'contact'
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => 'pages', :action => 'home'
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
 end
