f4efdce57fb65c1913095b26a3eb8049c9f5c14a
Andrew Shu <talklittle@gmail.com>
2010-11-23 12:02:57 -0500

    models/user: fix the has_many to DataMapper has n

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,12 @@ class User
   property :requester_score,     Integer,       :required => true
   timestamps :at 
 
-  has_many :network
-  has_many :system
-  has_many :task
-  has_many :script
-  has_many :comment
-  has_many :test_output
+  has n, :network
+  has n, :system
+  has n, :task
+  has n, :script
+  has n, :comment
+  has n, :test_output
 
   def password=(password)
     @password = password
