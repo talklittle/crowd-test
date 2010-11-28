9176ba33529e46d377c34d2c4872a5fb41f623c0
Andrew Shu <talklittle@gmail.com>
2010-11-28 01:21:32 -0500

    remove OS stuff. use tags instead

--- app/models/os.rb
+++
@@ -1,9 +0,0 @@
-class OS
-  include DataMapper::Resource
-  storage_names[:default] = "OS"
-  
-  property :id,      Serial
-  property :name,    String,        :required => true, :length => 500
-  property :version, String,        :required => true, :length => 500
-  timestamps :at 
-end
